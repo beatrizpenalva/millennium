96d9aa88eb3dbc022b412dcb04d8fcceb5030fd1
Beatriz Penalva <biapenalva@gmail.com>
2020-11-04 23:57:15 -0300

    style css

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 import cipher from './cipher.js';
-console.log(cipher);
-//evento do click para acionar a função - antes não precisava por que?
 document.getElementById("encode").addEventListener('click', printTextCoded);
-//função para mandar pro cipher os parâmetros da função encode e printar na tela o resultado
 function printTextCoded() {
-    //declarar a variável messageToEncode que é um dos parâmetros da função cipher e pegar por ID o valor do input
-    let messageToEncode = document.getElementById("textToEncode").value;
-    //garantir que as letras estarão em letra maiúscula por conta do intervalo do código ASCII
-    messageToEncode = messageToEncode.toUpperCase();
-    //pegar o valor do offset do input pra mandar pra função cipher (o segundo parâmetro)
     const offset = Number(document.getElementById("offset").value);
-    //enviando os valores do input como parâmetros da função cipher
-    const encodedMessage = cipher.encode (messageToEncode, offset);
-    //printando na tela a mensagem codificada
+    const messageToEncode = document.getElementById("textToEncode").value.toUpperCase();
+    const encodedMessage = cipher.encode(offset, messageToEncode);
     document.getElementById("textResult").innerText = encodedMessage;
 }
-//evento do click para acionar a função - antes não precisava por que?
 document.getElementById("decode").addEventListener('click', printTextDecoded);
-//criar a função pra disparar o botão de decifrar, outra forma de fazer isso seria usando eventos
 function printTextDecoded() {
-    //declarar a variável messageToEncode que é um dos parâmetros da função cipher e pegar por ID o valor do input
-    let messageToEncode = document.getElementById("textToEncode").value;
-    //garantir que as letras estarão em letra maiúscula por conta do intervalo do código ASCII
-    messageToEncode = messageToEncode.toUpperCase();
-    //pegar o valor do offset do input pra mandar pra função cipher (o segundo parâmetro)
     const offset = Number(document.getElementById("offset").value);
-    //enviando os valores do input como parâmetros da função cipher
-    const decodedMessage = cipher.decode (messageToEncode, offset);
-    //printando na tela a mensagem codificada
+    const messageToEncode = document.getElementById("textToEncode").value.toUpperCase();
+    const decodedMessage = cipher.decode(offset, messageToEncode);
     document.getElementById("textResult").innerText = decodedMessage;
+}
+document.getElementById("copyText").addEventListener('click', copyTextResult);
+function copyTextResult() {
+    const messageToCopy = document.getElementById("textResult").select();
+    document.execCommand("copy");
+}
+document.getElementById("clear").addEventListener('click', clearText);
+function clearText() {
+    document.getElementById("textToEncode").value = "";
+}
+document.getElementById("restart").addEventListener('click', restartCipher);
+function restartCipher(){
+    document.getElementById("textResult").value = "";
+    document.getElementById("textToEncode").value = "";
+    document.getElementById("offset").value = "";
 }
