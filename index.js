5f5ab4e069d1fffec2fd1379e7b66e10e364b809
Beatriz Penalva <biapenalva@gmail.com>
2021-04-13 14:53:12 -0300

    criação de nova página e nova estilização do app

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-import cipher from './cipher.js';
-document.getElementById("encode").addEventListener('click', printTextCoded);
+import cipher from "./cipher.js";
+const errorOffset = "Please, type a number."
+const errorText = "Please, type a text."
+
+function getOffset() {
+    const offset = Number(document.getElementById("offset").value);
+    if (offset !== 0 && offset !== null) {
+        return offset;
+    } else {
+        return console.log(errorOffset);
+    }
+}
+function getText() {
+    const messageToEncode = document.getElementById("message").value.toUpperCase();
+    if (messageToEncode !== "" && messageToEncode !== 0) {
+        return messageToEncode;
+    } else {
+        return console.log(errorText);
+    }
+}
+
+const offset = getOffset();
+const messageToEncode = getText();
+
+document.getElementById("button-encode").addEventListener("click", printTextCoded);
 function printTextCoded() {
-    const offset = Number(document.getElementById("offset").value);
-    const messageToEncode = document.getElementById("textToEncode").value.toUpperCase();
     const encodedMessage = cipher.encode(offset, messageToEncode);
-    document.getElementById("textResult").innerText = encodedMessage;
+    document.getElementById("final-message").innerText = encodedMessage;
 }
-document.getElementById("decode").addEventListener('click', printTextDecoded);
-function printTextDecoded() {
-    const offset = Number(document.getElementById("offset").value);
-    const messageToEncode = document.getElementById("textToEncode").value.toUpperCase();
+
+document.getElementById("button-decode").addEventListener("click", getAndPrintTextDecoded);
+function getAndPrintTextDecoded() {
     const decodedMessage = cipher.decode(offset, messageToEncode);
-    document.getElementById("textResult").innerText = decodedMessage;
+    document.getElementById("final-message").innerText = decodedMessage;
 }
-document.getElementById("copyText").addEventListener('click', copyTextResult);
+
+document.getElementById("button-copy").addEventListener("click", copyTextResult);
 function copyTextResult() {
-    document.getElementById("textResult").select();
+    document.getElementById("final-message").select();
     document.execCommand("copy");
+    document.getElementById("button-copy").innerHTML = "COPIED";
 }
-document.getElementById("clean").addEventListener('click', cleanText);
+
+document.getElementById("button-clean").addEventListener("click", cleanText);
 function cleanText() {
-    document.getElementById("textToEncode").value = "";
+    document.getElementById("message").value = "";
     document.getElementById("offset").value = "";
-}
-document.getElementById("restart").addEventListener('click', restartCipher);
-function restartCipher() {
-    document.getElementById("textResult").value = "";
-    document.getElementById("textToEncode").value = "";
-    document.getElementById("offset").value = "";
+    document.getElementById("final-message").innerHTML = "";
 }
