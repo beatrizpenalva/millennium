5aadeb879f23d85caf42308af85b5e56fdc42e70
Beatriz Penalva <biapenalva@gmail.com>
2021-04-24 17:59:11 -0300

    responsividade ajustada da página cipher

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import cipher from "../../cipher.js";
+import cipher from "./cipher.js";
 
 const getEncryptOptions = document.getElementsByName("method");
 const initialMessage = document.querySelector("#message");
@@ -8,18 +8,17 @@ const cleanButton = document.querySelector("#button-clean");
 const countCaracteres = document.querySelector("#count-caracteres");
 const getOffsetNumber = localStorage.getItem("offsetNumber");
 
+copyButton.addEventListener("click", copyFinalMessage);
+cleanButton.addEventListener("click", cleanFinalMessage);
 initialMessage.addEventListener("input", (e) => {
   e.preventDefault();
   countCaracteres.innerText = initialMessage.value.length;
   encryptMessage();
 });
 
-copyButton.addEventListener("click", copyFinalMessage);
-cleanButton.addEventListener("click", cleanFinalMessage);
-
 function encryptMessage() {
   const messageEncrypted = cipher.encrypt(
-    getOffsetNumber(Math.abs(Number("offsetNumber"))),
+    Math.abs(Number(getOffsetNumber)),
     initialMessage.value,
     getEncryptOptions[0].checked ? "encode" : "decode"
   );
