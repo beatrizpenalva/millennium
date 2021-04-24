57235fa62534fe520509a6b734e769d004b4c7dc
Beatriz Penalva <biapenalva@gmail.com>
2021-04-24 20:43:38 -0300

    ajustes de usabilidade dos inputs encode/decode

--- src/main.js
+++ src/main.js
@@ -7,25 +7,29 @@ const copyButton = document.querySelector("#button-copy");
 const cleanButton = document.querySelector("#button-clean");
 const countCaracteres = document.querySelector("#count-caracteres");
 const getOffsetNumber = localStorage.getItem("offsetNumber");
+const encodeButton = document.querySelector("#encode");
+const decodeButton = document.querySelector("#decode");
 
+encodeButton.addEventListener("change", encryptMessage);
+decodeButton.addEventListener("change", encryptMessage);
+initialMessage.addEventListener("input", encryptMessage);
 copyButton.addEventListener("click", copyFinalMessage);
 cleanButton.addEventListener("click", cleanFinalMessage);
-initialMessage.addEventListener("input", (e) => {
-  e.preventDefault();
-  countCaracteres.innerText = initialMessage.value.length;
-  encryptMessage();
-});
 
 function encryptMessage() {
+  countCaracteres.innerText = initialMessage.value.length;
+
   const messageEncrypted = cipher.encrypt(
     Math.abs(Number(getOffsetNumber)),
     initialMessage.value,
     getEncryptOptions[0].checked ? "encode" : "decode"
   );
+
   printMessage(messageEncrypted);
 }
 
 function printMessage(messageEncrypted) {
+  document.querySelector("#final-message").innerText = "";
   document.querySelector("#final-message").innerText = messageEncrypted;
 }
 
@@ -33,10 +37,12 @@ function copyFinalMessage() {
   finalMessage.select();
   document.execCommand("copy");
   document.querySelector("#button-copy").innerHTML = "COPIED";
+  setTimeout(function () {
+    document.querySelector("#button-copy").innerHTML = "COPY";
+  }, 1000);
 }
 
 function cleanFinalMessage() {
   document.querySelector("#message").value = "";
-  document.querySelector("#offset").value = "";
   document.querySelector("#final-message").innerHTML = "";
 }
